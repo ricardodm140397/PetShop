471e3a98a8750e6bdb853c2afbad00a561ea46c2
unknown <angel_95pl@hotmail.com>
2020-04-15 04:19:22 -0500

    carrito de compras

--- JS/home/Home.js
+++ JS/home/Home.js
@@ -1,5 +1,25 @@
+/* import { Api } from '../services/ApiConsumer';
+const api = Api(); */
 export class Home {
   constructor() {
-    console.log("Home");
+    this.createShoppingCard();
+  }
+
+  createShoppingCard() {
+    const carShopping = document
+      .querySelector("#buttonShoppingCar")
+      .addEventListener("mouseover", () => {
+        const showCarShopping = document.querySelector("#carShoppingCard");
+        showCarShopping.innerHTML = `<li class="collection-item avatar">
+                                      <img src="img/93154211_240902656962032_510511237415567360_n.png" alt="" class="circle">
+                                        <span class="title">Title</span>
+                                          <p>First Line <br>
+                                                 Second Line
+                                          </p>
+                                      <a href="#!" class="secondary-content"><i class="material-icons">attach_money</i></a>
+                                    </li>`;
+      });
   }
 }
+
+const render = new Home();
